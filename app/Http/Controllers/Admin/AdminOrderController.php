d19fa4e6088980e07b78186135ff2a8b7a5c7cda
Mark James <markjamesdelasalas@gmail.com>
2026-08-12 17:26:26 +0800

    bug fixed orders

--- app/Http/Controllers/Admin/AdminOrderController.php
+++ app/Http/Controllers/Admin/AdminOrderController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateOrderStatusRequest;
 use App\Models\Order;
 use App\Services\OrderService;
+use Illuminate\Http\Request;
 
 class AdminOrderController extends Controller
 {
