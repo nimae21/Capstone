bb608cd7c27b33e535a301f34bed510a4ce6aa16
Mark James <markjamesdelasalas@gmail.com>
2026-09-06 00:19:05 +0800

    force persistent production sessions

--- config/session.php
+++ config/session.php
@@ -18,7 +18,9 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', env('APP_ENV') === 'production' ? 'file' : 'database'),
+    'driver' => env('APP_ENV') === 'production'
+        ? 'file'
+        : env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
