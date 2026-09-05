970655e9cf2f50684459806228902daaaee2b725
Mark James <markjamesdelasalas@gmail.com>
2026-09-06 00:33:45 +0800

    attach queued session cookies to web responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,8 @@
 
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\IsUser;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
         $middleware->web(prepend: [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
             StartSession::class,
         ]);
 
