7ad98297dde025310101b2568e2574a3ad0c15d8
Mark James <markjamesdelasalas@gmail.com>
2026-09-06 00:52:34 +0800

    putang inamo

--- config/session.php
+++ config/session.php
@@ -18,9 +18,7 @@ return [
     |
     */
 
-    'driver' => env('APP_ENV') === 'production'
-        ? 'file'
-        : env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
